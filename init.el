12d3b9ae8819154dfb0f92e1491dd4dc0481ed77
Eduardo Pereira Poernbacher <hey@drgmr.com>
2024-09-02 11:24:28 -0300

    hello again

--- init.el
+++ init.el
@@ -1,4 +1,3 @@
-;; Set up package management
 (setq straight-bootstrap-file (expand-file-name "var/straight/repos/straight.el/bootstrap.el" user-emacs-directory)
       straight-installer-url "https://raw.githubusercontent.com/raxod502/straight.el/develop/install.el")
 
@@ -19,18 +18,11 @@
       use-package-always-demand t)
 (straight-use-package 'use-package)
 
-;; PACKAGES
+(use-package straight
+  :custom
+  (straight-built-in-pseudo-packages '(emacs nadvice python image-mode project flymake xref)))
 
-;; Core
 (use-package no-littering)
-
-(use-package exec-path-from-shell
-  :config
-  (setq exec-path-from-shell-shell-name (executable-find "zsh"))
-  (setq exec-path-from-shell-variables '("PATH"))
-  (exec-path-from-shell-initialize))
-
-;; (use-package kaolin-themes)
 
 (use-package emacs
   :init
@@ -40,7 +32,8 @@
     (global-visual-line-mode)
     (tooltip-mode -1)
     (set-face-attribute 'default nil :font "Berkeley Mono" :height 215)
-    (set-face-attribute 'fixed-pitch nil :font "Berkeley Mono"))
+    (set-face-attribute 'fixed-pitch nil :font "Berkeley Mono")
+    (set-fontset-font "fontset-default" 'unicode "Apple Color Emoji" nil 'prepend))
 
   :config
   (progn
@@ -48,30 +41,38 @@
       "Load theme, taking current system APPEARANCE into consideration."
       (mapc #'disable-theme custom-enabled-themes)
       (pcase appearance
-	('light (load-theme 'modus-operandi-tinted t))
-	('dark (load-theme 'modus-vivendi-tinted t))))
+      	('light (load-theme 'modus-operandi-tinted t))
+      	('dark (load-theme 'modus-vivendi-tinted t))))
+
     (defun drgmr/add-list-to-list (dst src)
       "Similar to `add-to-list', but accepts a list as 2nd argument"
       (set dst (append (eval dst) src)))
 
     (setq load-prefer-newer t
           visible-bell t
-	  inhibit-startup-message t
-	  auto-save-default nil
-	  make-backup-files nil
-	  frame-resize-pixelwise t
-	  browse-url-browser-function 'xwidget-webkit-browse-url
-	  custom-file (no-littering-expand-etc-file-name "custom.el")
-	  scheme-program-name "csi -:c"
-	  whitespace-style '(face trailing)
+      	  inhibit-startup-message t
+      	  auto-save-default nil
+      	  make-backup-files nil
+      	  frame-resize-pixelwise t
+      	  browse-url-browser-function 'xwidget-webkit-browse-url
+      	  custom-file (no-littering-expand-etc-file-name "custom.el")
+      	  whitespace-style '(face trailing)
           css-indent-offset 2
           gc-cons-threshold 100000000
           read-process-output-max (* 1024 1024)
           mac-right-option-modifier 'none
           js-indent-level 2
           native-comp-async-report-warnings-errors 'silent)
+
     (setq-default indent-tabs-mode nil
-                  indicate-empty-lines t))
+                  indicate-empty-lines t)
+
+    (setq treesit-language-source-alist
+          '((elixir . ("https://github.com/elixir-lang/tree-sitter-elixir.git" "v0.2.0"))
+            (heex . ("https://github.com/phoenixframework/tree-sitter-heex.git" "v0.6.0")))
+          major-mode-remap-alist
+          '((elixir-mode . elixir-ts-mode)
+            (heex-mode . heex-ts-mode))))
 
   :hook
   (prog-mode-hook . subword-mode)
@@ -92,59 +93,6 @@
    ("s-<up>" . windmove-up)
    ("s-<down>" . windmove-down)))
 
-(use-package web-mode
-  :config
-  (setq web-mode-markup-indent-offset 2
-        web-mode-css-indent-offset 2
-        web-mode-code-indent-offset 2
-        web-mode-sql-indent-offset 2
-        web-mode-enable-current-element-highlight t
-        web-mode-start-tag-regexp "<\\([[:alpha:]][[:alnum:].:_-\.]*\\|>\\)"
-        web-mode-tag-regexp "</?\\([[:alpha:]][[:alnum:].:_-\.]*\\)"))
-
-(use-package org
-  :config
-  (progn
-    (setq org-catch-invisible-edits 'error
-          org-agenda-files '("~/Projects/roam")
-          org-pretty-entities t
-          org-pretty-entities-include-sub-superscripts t
-          org-use-sub-superscripts t
-          org-return-follows-link t
-          org-todo-keywords '((sequence "TODO(t)" "NEXT(n)" "WAITING(w)" "|" "DONE(d)" "CANCELLED(c)")))
-    (add-to-list 'org-link-frame-setup '(file . find-file))
-    (org-babel-do-load-languages
-     'org-babel-load-languages
-     (append org-babel-load-languages
-             '((scheme . t)
-               (dot . t)))))
-  :hook
-  (org-mode-hook . org-indent-mode))
-
-(use-package org-roam
-  :init
-  (setq org-roam-v2-ack t)
-  :custom
-  (org-roam-directory "~/Projects/roam/")
-  (org-roam-complete-everywhere t)
-  (org-roam-capture-templates
-   '(("d" "default" plain
-      "%?"
-      :target (file+head "%<%Y%m%d%H%M%S>-${slug}.org" "#+title: ${title}\n")
-      :unnarrowed t)
-     ("b" "book notes" plain
-      "\n\n* Source\n\nAuthor: %^{Author}\nTitle: ${title}\nYear: %^{Year}\n\n* Summary\n\n%?"
-      :target (file+head "%<%Y%m%d%H%M%S>-${slug}.org" "#+title: ${title}\n")
-      :unnarrowed t)))
-  :config
-  (org-roam-setup)
-  :bind
-  (("C-c n l" . org-roam-buffer-toggle)
-   ("C-c n f" . org-roam-node-find)
-   ("C-c n i" . org-roam-node-insert)
-   ("C-c n t" . org-roam-dailies-goto-today)
-   ("C-c n y" . org-roam-dailies-goto-yesterday)))
-
 (use-package marginalia
   :init
   (marginalia-mode)
@@ -160,8 +108,9 @@
 (use-package projectile
   :config
   (progn
-    (setq projectile-project-search-path '("~/Projects" "~/quicklisp/local-projects")
-	  projectile-switch-project-action 'magit-status)
+    (setq projectile-project-search-path '("~/Projects")
+      	  projectile-switch-project-action 'magit-status
+          projectile-kill-buffers-filter 'kill-only-files)
     (add-to-list 'projectile-other-file-alist '("ex" "html.heex"))
     (add-to-list 'projectile-other-file-alist '("html.heex" "ex")))
 
@@ -174,14 +123,6 @@
 (use-package docker
   :bind
   (("C-x d" . docker)))
-
-(use-package kubernetes
-  :commands (kubernetes-overview)
-  :config
-  (setq kubernetes-poll-frequency 3600
-        kubernetes-redraw-frequency 3600)
-  :bind
-  (("C-x k" . kubernetes-overview)))
 
 (use-package selectrum
   :hook
@@ -197,8 +138,6 @@
   (setq hungry-delete-join-reluctantly t)
   :hook
   (prog-mode-hook . hungry-delete-mode))
-
-;; Programming utilities
 
 (use-package company
   :diminish
@@ -246,54 +185,17 @@
   :bind
   (("C-x g" . magit-status)))
 
-(use-package ctrlf
-  :init
-  (ctrlf-mode))
-
-;; Languages
-
-(use-package elixir-mode
-  :config
-  (yas-define-snippets 'elixir-mode
-                       '(("debug" "IO.inspect(binding(), label: \"#{__ENV__.module}.#{elem(__ENV__.function, 0)}/#{elem(__ENV__.function, 1)}\")")
-                         ("pry" "require IEx; IEx.pry()"))))
+(use-package elixir-mode)
 
 (use-package exunit
   :hook
-  (elixir-mode-hook . exunit-mode))
-
-(use-package flycheck-credo
-  :hook
-  (elixir-mode-hook . flycheck-mode))
+  (elixir-ts-mode-hook . exunit-mode))
 
 (use-package erlang)
 
 (use-package yaml-mode)
 
-(use-package rust-mode)
-
 (use-package tree-sitter-indent)
-
-(use-package lfe-mode)
-
-(use-package clojure-mode)
-
-(use-package cider)
-
-(use-package sly
-  :config
-  (setq inferior-lisp-program "sbcl")
-  :hook
-  (lisp-mode-hook . paredit-mode))
-
-(use-package paredit
-  :hook
-  (common-lisp-mode-hook . paredit-mode)
-  (clojure-mode-hook . paredit-mode))
-
-(use-package zig-mode)
-
-;; Other utilities
 
 (use-package markdown-mode
   :commands
@@ -309,35 +211,7 @@
 
 (use-package ripgrep)
 
-(use-package org-tree-slide
-  :custom
-  (org-image-actual-width nil))
-
-(use-package geiser-chicken)
-
-(use-package org-roam-ui
-  :straight
-  (:host github :repo "org-roam/org-roam-ui" :branch "main" :files ("*.el" "out"))
-  :after org-roam
-  :config
-  (setq org-roam-ui-sync-theme t
-        org-roam-ui-follow t
-        org-roam-ui-update-on-save t
-        org-roam-ui-open-on-start t))
-
-(use-package org-contrib)
-
 (use-package protobuf-mode)
-
-(use-package yasnippet
-  :hook
-  (prog-mode-hook . yas-minor-mode)
-  :config
-  (add-to-list 'company-backends 'company-yasnippet))
-
-(use-package yasnippet-snippets)
-
-(use-package swift-mode)
 
 (use-package envrc
   :config
@@ -356,59 +230,6 @@
           '((nil . (telephone-line-misc-info-segment))
             (accent . (telephone-line-airline-position-segment))))))
 
-(use-package lsp-mode
-  :commands (lsp lsp-deferred)
-  :config
-  (setq lsp-file-watch-threshold nil)
+(use-package eglot
   :hook
-  ((elixir-mode-hook . lsp-deferred)
-   (swift-mode-hook . lsp-deferred)
-   (zig-mode-hook . lsp-deferred)
-   (rust-mode-hook . lsp-deferred)
-   (purescript-mode-hook . lsp-deferred)
-   (erlang-mode-hook . lsp-deferred)
-   (lsp-mode-hook . lsp-enable-which-key-integration)
-   (lsp-mode-hook . lsp-ui-mode)))
-
-(use-package lsp-ui
-  :commands (lsp-ui-mode lsp-ui-imenu)
-  :config
-  (setq lsp-ui-doc-show-with-cursor t
-        lsp-ui-doc-show-with-mouse t)
-  :bind
-  (("C-'" . lsp-ui-imenu)))
-
-(use-package dap-mode)
-
-(require 'dap-elixir)
-(require 'dap-erlang)
-(require 'dap-gdb-lldb)
-(require 'dap-cpptools)
-
-(dap-register-debug-template "Rust::GDB Run Configuration"
-                             (list :type "gdb"
-                                   :request "launch"
-                                   :name "GDB::Run"
-                                   :gdbpath "rust-gdb"
-                                   :target nil
-                                   :cwd nil))
-(use-package wgsl-mode
-  :straight (:host github :repo "acowley/wgsl-mode"))
-
-(use-package purescript-mode
-  :hook
-  (purescript-mode-hook . purescript-indentation-mode))
-
-(use-package dhall-mode
-  :mode "\\.dhall\\'")
-
-(use-package haskell-mode)
-
-(use-package dante
-  :commands 'dante-mode
-  :init
-  (setq dante-repl-command-line '("ghci")
-        haskell-process-type 'ghci)
-  :hook
-  ((haskell-mode-hook . dante-mode)
-   (haskell-mode-hook . flycheck-mode)))
+  (elixir-ts-mode-hook . eglot-ensure))
